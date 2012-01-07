c9e4dc86da6d286dead4431f94a17de0c6913404
Brett Fowle <brettfowle@gmail.com>
2012-01-07 17:27:34 -0500

    Self-instantitating, anonymous function

--- swap.js
+++ swap.js
@@ -21,55 +21,57 @@ var BFJS = this.BFJS = {};
  *				BFJS.swap(el1, el2, {hideOld: false, hideNew: false});
  *				BFJS.swap(el1, el2, {durOld: 300, durNew: 800});
  */
-BFJS.swap = function(oldEl, newEl, options) {
-	var options = options || {},
-		/**
-		 * options
-		 */
-		options = {
-			hideOld: options.hideOld || true,
-			hideNew: options.hideNew || true,
-			durOld:	options.durOld || 500,
-			durNew: options.durNew || 500
-		},
-		/**
-		 * offFunction
-		 */
-		offFunction = function(el, fn) {
-			el.set('morph', {duration: options.durOld})
-				.morph({'opacity': 0})
-				.get('morph')
-				.chain(function() {
-					if (options.hideOld) el.setStyle('display', 'none');
-				});
-		},
-		/**
-		 * onFunction
-		 */
-		onFunction = function(el, fn) {
-			if (options.hideNew) {
-				el.setStyles({
-					'display': 'block',
-					'opacity': 0
-				});
-			}
-			el.set('morph', {duration: options.durNew || 500})
-				.morph({'opacity': 1});
-		};
+BFJS.swap = (function() {
+	return function(oldEl, newEl, options) {
+		var options = options || {},
+			/**
+			 * options
+			 */
+			options = {
+				hideOld: options.hideOld || true,
+				hideNew: options.hideNew || true,
+				durOld:	options.durOld || 500,
+				durNew: options.durNew || 500
+			},
+			/**
+			 * offFunction
+			 */
+			offFunction = function(el, fn) {
+				el.set('morph', {duration: options.durOld})
+					.morph({'opacity': 0})
+					.get('morph')
+					.chain(function() {
+						if (options.hideOld) el.setStyle('display', 'none');
+					});
+			},
+			/**
+			 * onFunction
+			 */
+			onFunction = function(el, fn) {
+				if (options.hideNew) {
+					el.setStyles({
+						'display': 'block',
+						'opacity': 0
+					});
+				}
+				el.set('morph', {duration: options.durNew || 500})
+					.morph({'opacity': 1});
+			};
+			
+		if (typeOf(oldEl) == 'elements') {
+			oldEl.each(function(el) {
+				offFunction(el);
+			});
+		} else if (typeOf(oldEl) == 'element') {
+			offFunction(oldEl);
+		}
 		
-	if (typeOf(oldEl) == 'elements') {
-		oldEl.each(function(el) {
-			offFunction(el);
-		});
-	} else if (typeOf(oldEl) == 'element') {
-		offFunction(oldEl);
-	}
-	
-	if (typeOf(newEl) == 'elements') {
-		newEl.each(function(el) {
-			onFunction(el);
-		});
-	} else if (typeOf(newEl) == 'element') {
-		onFunction(newEl);
-	}
-};
+		if (typeOf(newEl) == 'elements') {
+			newEl.each(function(el) {
+				onFunction(el);
+			});
+		} else if (typeOf(newEl) == 'element') {
+			onFunction(newEl);
+		}
+	};
+})();
